6ba8f298df37455db40f96d8ccc436099a00cbd8
Abdul Rehman <arrehman0r@gmail.com>
2024-10-18 10:58:24 +0500

    added medusa

--- App.js
+++ App.js
@@ -3,21 +3,27 @@ import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import AppToast from './components/AppToast';
 import AppLoader from './components/AppLoader';
 import Routes from './routes/routes';
-
+import { QueryClient } from "@tanstack/react-query"
+import { MedusaProvider } from "medusa-react"
 export default function App() {
+  const queryClient = new QueryClient()
   return (
     <SafeAreaProvider>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Routes />
-          <AppToast />
-          <AppLoader />
-        </PersistGate>
-      </Provider>
+      <MedusaProvider
+        queryClientProviderProps={{ client: queryClient }}
+        baseUrl="https://pizza.al-mizan.store"
+      >
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Routes />
+            <AppToast />
+            <AppLoader />
+          </PersistGate>
+        </Provider>
+      </MedusaProvider>
     </SafeAreaProvider>
   );
 }
